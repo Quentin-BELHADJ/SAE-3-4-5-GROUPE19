f793fe641a17345b7d090aaae585adf4688a79a2
Audrick <audrick.soltner@gmail.com>
2024-01-25 17:05:12 +0100

    push

--- connexion_db.py
+++ connexion_db.py
@@ -8,7 +8,7 @@ def get_db():
         #
         db = g._database = pymysql.connect(
             host="localhost",
-            user="bidule",
+            user="audrick",
             password="mdp",
             database="S2",
             charset='utf8mb4',
